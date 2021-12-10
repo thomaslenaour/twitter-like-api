d0c9f7f44ac41356172685727d25fa88b6169196
Alex Boisseau <alex.boisseau79@gmail.com>
2021-12-10 14:54:14 +0100

    feat(tweet): add service and repository in providers array for the tweet module

--- src/business/tweet/tweet.module.ts
+++ src/business/tweet/tweet.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TweetRepository } from './tweet.repository';
+import { TweetService } from './tweet.service';
 
 @Module({
-  providers: [],
+  providers: [TweetService, TweetRepository],
 })
 export class TweetModule {}
